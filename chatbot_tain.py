15cc86dc8900726dc3a8d14ea70bce61eabb8e3d
samIM2627 <37177797+samIM2627@users.noreply.github.com>
2018-08-29 10:44:54 +0530

    Add files via upload

---
+++ chatbot_tain.py
@@ -0,0 +1,18 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Corpus')
+bot.set_trainer(ListTrainer)
+
+for files in os.listdir('C:/Users/smriti.maithani\Documents\Chatbot\chatterbot-corpus-master\chatterbot_corpus\data\english/'):
+        data = open('C:/Users/smriti.maithani\Documents\Chatbot\chatterbot-corpus-master\chatterbot_corpus\data\english/' + files , 'r').readlines()
+        bot.train(data)
+while True:
+        message = input('You:')
+        if message.strip() != 'Bye':
+                reply = bot.get_response(message)
+                print('ChatBot :',reply)
+        if message.strip() == 'Bye':
+                print('ChatBot : Bye')
+                break
